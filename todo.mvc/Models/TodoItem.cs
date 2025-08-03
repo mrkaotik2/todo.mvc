fc0038beac5187b87bf29b7c330ee36f6b0d0616
Kris Norris <kristophergraves@hotmail.com>
2025-08-03 17:52:52 -0500

    Added TodoItem Model

---
+++ todo.mvc/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace todo.mvc.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool IsComplete { get; set; }
+
+}
