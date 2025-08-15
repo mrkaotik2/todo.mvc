174c5eb560cf0dec6a3d4c91f69c5b3d3993b7ca
Kris Norris <kristophergraves@hotmail.com>
2025-08-14 20:34:55 -0500

    Added Create, Delete, Edit and Index (CRUD)

---
+++ todo.mvc/Controllers/ToDoController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using todo.mvc.Data;
+using todo.mvc.Models;
+
+public class TodoController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public TodoController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var todos = await _context.TodoItems.ToListAsync();
+        return View(todos);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(TodoItem item)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(item);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(item);
+    }
+
+    public async Task<IActionResult> Edit(int id)
+    {
+        var item = await _context.TodoItems.FindAsync(id);
+        if (item == null) return NotFound();
+        return View(item);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, TodoItem item)
+    {
+        if (id != item.Id) return NotFound();
+
+        if (ModelState.IsValid)
+        {
+            _context.Update(item);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(item);
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var item = await _context.TodoItems.FindAsync(id);
+        if (item == null) return NotFound();
+        return View(item);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var item = await _context.TodoItems.FindAsync(id);
+        if (item != null)
+        {
+            _context.TodoItems.Remove(item);
+            await _context.SaveChangesAsync();
+        }
+        return RedirectToAction(nameof(Index));
+    }
+}
--- todo.mvc/Program.cs
+++ todo.mvc/Program.cs
@@ -35,6 +35,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=ToDo}/{action=Index}/{id?}");
 
 app.Run();
